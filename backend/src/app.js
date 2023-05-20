d8870c0892267b2a75ecf93a6361a7d0b104cf62
JP <jp02120123@gmail.com>
2023-05-20 17:31:43 -0300

    feat: add cors api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from 'cors';
 import productsRouter from "./routes/productsRouter.js"
 import clientsRouter from "./routes/clientRouter.js"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({"hello": 'Welcome to our products api'})
